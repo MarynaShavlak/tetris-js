a5c288cfc9e5b9a559379d9fc170fb1c8af9e963
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-29 21:05:57 +0300

    add controls buttons

--- js/canvas/canvas.js
+++ js/canvas/canvas.js
@@ -1,8 +1,6 @@
 import {drawCircle} from "./drawCircle.js";
-import {drawDoubleCircleAt90} from "./drawDoubleCircleAt90.js";
-import {drawSecondCirclePart} from "./drawSecondCirclePart.js";
-import {drawDottedCircle} from "./drawDottedCircle.js";
 
-drawCircle();
 
-// drawDoubleCircleAt90();
+// drawCircle();
+
+
