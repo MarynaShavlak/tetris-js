21861edcf1178ac6bea4860d5c5f2fa08637167c
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-30 12:08:36 +0300

    add code for show instructions

--- js/eventHandlers/rules.js
+++ js/eventHandlers/rules.js
@@ -1,8 +1,16 @@
-import {rulesEl, startEl, usernameEl} from "../index.js";
+export const usernameEl = document.querySelector('.settings__item--username');
+export const startEl = document.querySelector('.settings__item--start');
+export const rulesEl = document.querySelector('.settings__item--rules');
+export const instructionsEl = document.querySelector('.instructions');
 
 export function handleShowRules() {
     console.log('show rules')
     usernameEl.classList.add('hidden');
     startEl.classList.add('hidden');
     rulesEl.style.order = -1;
+    setTimeout(() => {
+        instructionsEl.classList.remove('hidden');
+    }, 300);
+
+
 }
--- js/index.js
+++ js/index.js
@@ -2,10 +2,7 @@ import {handleShowRules} from "./eventHandlers/rules.js";
 
 
 const showRulesBtn = document.getElementById('rules-btn');
-export const usernameEl = document.querySelector('.settings__item--username');
-export const startEl = document.querySelector('.settings__item--start');
-export const rulesEl = document.querySelector('.settings__item--rules');
-export const instructionsEl = document.querySelector('.instructions');
+
 showRulesBtn.addEventListener('click', handleShowRules);
 
 
