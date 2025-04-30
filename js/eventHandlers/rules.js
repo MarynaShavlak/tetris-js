c38e3167393c9c0ee629f6b058de00c58bbe31e6
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-30 12:21:16 +0300

    add code for control buttons

--- js/eventHandlers/rules.js
+++ js/eventHandlers/rules.js
@@ -2,6 +2,7 @@ export const usernameEl = document.querySelector('.settings__item--username');
 export const startEl = document.querySelector('.settings__item--start');
 export const rulesEl = document.querySelector('.settings__item--rules');
 export const instructionsEl = document.querySelector('.instructions');
+const settingsFooterEl = document.querySelector('.settings__footer');
 
 export function handleShowRules() {
     console.log('show rules')
@@ -10,6 +11,7 @@ export function handleShowRules() {
     rulesEl.style.order = -1;
     setTimeout(() => {
         instructionsEl.classList.remove('hidden');
+        settingsFooterEl.classList.remove('hidden');
     }, 300);
 
 
