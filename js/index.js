8c35a5b42137ac804ec9d14c1f0eac32ccc63f45
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-01 18:02:12 +0300

    add code for goals fixes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,6 @@
-import {  TetrisGame } from "./config/gameConfig.js";
 import {
   drawFieldNewState,
-  dropTetro,
-  moveTetroDown,
-  rotateTetro,
-  updateGameState,
-  moveTetroHorizontally,
+
 } from "./core/tetro/tetrominoLogic.js";
 
 import {setupEventListeners} from "./core/events/setupEventListeners.js";
